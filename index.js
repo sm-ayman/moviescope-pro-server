5f8958079d37584060e7bc23e47df473902b5672
sm_ayman <sultanmdayman@gmail.com>
2025-11-09 16:05:07 +0600

    server updated

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
-import dotenv from "dotenv";
-dotenv.config();
+
 
 // middleware
 app.use(cors());
@@ -24,15 +24,14 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
