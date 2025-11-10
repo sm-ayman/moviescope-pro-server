05b643b291c1a4de7928b6a64b6fcb34c39c93c7
sm_ayman <sultanmdayman@gmail.com>
2025-11-10 14:48:39 +0600

    delete-movie api created

--- index.js
+++ index.js
@@ -42,6 +42,14 @@ async function run() {
       }
     });
 
+    // delete-movie
+    app.delete("/movies/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await moviesCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
