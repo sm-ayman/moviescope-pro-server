d876eb64f2969ce7f6e7f2531441add512489c5e
sm_ayman <sultanmdayman@gmail.com>
2025-11-10 14:49:04 +0600

    update-movie api created

--- index.js
+++ index.js
@@ -42,6 +42,14 @@ async function run() {
       }
     });
 
+    // update-movie
+    app.patch("/movies/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await moviesCollection.updateOne(query);
+      res.send(result);
+    });
+
     // delete-movie
     app.delete("/movies/:id", async (req, res) => {
       const id = req.params.id;
