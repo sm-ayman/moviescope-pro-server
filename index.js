d646bd1482e46c359061b59acb7daac5a0d5b288
sm_ayman <sultanmdayman@gmail.com>
2025-11-12 19:37:13 +0600

    Server updated

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     console.log("Connected to MongoDB");
 
     const db = client.db("moviescope_db");
